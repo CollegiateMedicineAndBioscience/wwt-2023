19ebdf3068e5dc9be74b30771e77b8ba7b316f26
C4 Patino <c4patino@gmail.com>
2023-02-03 14:50:38 -0600

    Beginnings of login/logout code

---
+++ server/routes/user/Logout.js
@@ -0,0 +1,20 @@
+const { User } = require('../../db/models/index');
+const errors = require('../../config/error.json');
+
+async function Logout(req, res) {
+    const { id } = req.query;
+
+    const result = await User.findByPk(id);
+
+    // Make sure that the result exists
+    if (!result) {
+        return res.status(404).send(errors.NotFound);
+    }
+
+    // Filter out things that shouldn't be sent to the frontend
+    const { salt, password, ...rest } = result.dataValues;
+
+    return res.send({ user: rest });
+}
+
+module.exports = Logout;
