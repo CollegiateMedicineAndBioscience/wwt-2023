56d3b0a8b8b9bd52c8df586f885f7bb737c79a9a
C4 Patino <c4patino@gmail.com>
2023-02-04 15:39:35 -0600

    Removed masterPassword implementation

--- server/routes/orgRouter.js
+++ server/routes/orgRouter.js
@@ -2,16 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
-const masterPassword = require('../middleware/masterPassword');
-
 const CreateOrganization = require('./organizations/CreateOrganization');
 const GetOrganization = require('./organizations/GetOrganization');
 const UpdateOrganization = require('./organizations/UpdateOrganization');
 const DeleteOrganization = require('./organizations/DeleteOrganization');
 
-router.post('/', masterPassword, CreateOrganization);
+router.post('/', CreateOrganization);
 router.get('/', GetOrganization);
-router.patch('/', masterPassword, UpdateOrganization);
-router.delete('/', masterPassword, DeleteOrganization);
+router.patch('/', UpdateOrganization);
+router.delete('/', DeleteOrganization);
 
 module.exports = router;
