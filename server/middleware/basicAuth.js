342bfefa5ebe5ea88210d851c5a56f71c2233b4a
gywn9081 <henry.bloch.04@gmail.com>
2023-02-07 20:04:55 -0600

    Fixing basic auth so that it now works.

--- server/middleware/basicAuth.js
+++ server/middleware/basicAuth.js
@@ -4,14 +4,19 @@ const { User } = require('../db/models/index');
 const errors = require('../config/error.json');
 
 async function basicAuth(req, res, next) {
-    const authHeader = req.get('Authorization');
-    const b64Encoded = authHeader?.split(' ')[1];
+    const { headers } = req;
 
-    if(!b64Encoded) return res.status(400).send(errors.Incomplete);
+    if (!headers) return res.status(400).send(errors.Incomplete);
+
+    const { authorization } = headers;
+
+    const b64Encoded = authorization?.split(' ')[1];
+
+    if (!b64Encoded) return res.status(400).send(errors.Incomplete);
 
     // Isolate username and password
     const [email, password] = Buffer.from(b64Encoded, 'base64').toString().split(':');
-    if(!email || !password) return res.status(400).send(errors.Incomplete);
+    if (!email || !password) return res.status(400).send(errors.Incomplete);
 
     // Make sure that a user exists with that email
     const result = await User.findOne({
@@ -22,9 +27,9 @@ async function basicAuth(req, res, next) {
     }
 
     // Hash the password and verify that the user hash is identical
-    const passwordHash = await argon2.hash(`${password}:${result.salt}`);
+    const passwordHash = await argon2.verify(result.password, `${password}:${result.salt}`);
 
-    if (passwordHash !== result.password) {
+    if (!passwordHash) {
         return res.status(403).send(errors.Forbidden);
     }
 
