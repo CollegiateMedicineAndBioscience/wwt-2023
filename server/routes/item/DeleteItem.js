4591e11951eeef0000fed88562258411dfb3cd38
Henry Bloch <henry.bloch.04@gmail.com>
2023-02-15 09:03:33 -0600

    Theoretically working delete items

--- server/routes/item/DeleteItem.js
+++ server/routes/item/DeleteItem.js
@@ -1,13 +1,51 @@
+const { Op } = require('sequelize');
+
 const { Item } = require('../../db/models/Item');
 const errors = require('../../config/error.json');
 const logger = require('../../utils/logger');
 
 async function DeleteItem(req, res) {
-    const { id } = req.query;
+    const { ids } = req.query;
     const { uid } = req.token.body;
 
-    if (!id) return res.status(400).send(errors.Incomplete);
-    console.log(id);
+    // Make sure that there is a id in the request
+    if (!ids || ids.length === 0) {
+        return res.status(400).send(errors.NotFound);
+    }
+    // Making sure items exist
+
+    const itemCount = await Item.findAll({
+        where: {
+            id: { [Op.in]: ids },
+        }
+    });
+    // Make sure all items are found
+    if (!(itemCount.length === ids.length)) {
+        return res.status(404).send(errors.NotFound);
+    }
+
+    // Make sure all items found belong to the owner passed
+    for (let i = 0; i < itemCount.length; i += 1) {
+        if (itemCount[i].owner !== uid) {
+            return res.status(403).send(errors.Forbidden);
+        }
+    }
+
+    //  deletion of records matching in array of ids
+    try {
+        await Item.destroy({
+            where: {
+                id: {
+                    [Op.in]: ids
+                }
+            }
+        });
+        return res.sendStatus(200);
+    } catch (e) {
+        logger.error(e);
+
+        return res.status(500).send(errors.Generic);
+    }
 }
 
 module.exports = DeleteItem;
